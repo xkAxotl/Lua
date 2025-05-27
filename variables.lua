78cd42646e8f5bf53d7d4e179f7dd137551504fa
jazAlonso <xkaxolt@outlook.com>
2025-05-26 21:02:29 -0600

    boolean variable example

---
+++ variables.lua
@@ -0,0 +1,27 @@
+-- nil represents an undefined value
+local a
+print(a)
+
+-- Declare a local variable named 'num' and assign it the value 20
+local num = 20
+
+-- Print the result of adding 1 to the value of 'num' (outputs 21)
+print(num + 1)
+
+-- Declare a locar variable and print a sentence using string concatenation
+
+local food = "pozole"
+print("I like " .. food)
+
+
+-- Combine first and last name using local variables and string concatenation
+local name = 'Daniel'
+local surname = ' Smith'
+
+local full_name = name .. "" .. surname
+
+print(full_name)
+
+-- Declare a local boolean variable and print its value
+local old = true
+print(old)
