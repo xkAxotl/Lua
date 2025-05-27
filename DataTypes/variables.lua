5ae153227b057c3ff18b611a6a9caba8f58777fd
jazAlonso <xkaxolt@outlook.com>
2025-05-26 20:47:04 -0600

    demonstrate local scope with print

--- DataTypes/variables.lua
+++ DataTypes/variables.lua
@@ -1,3 +1,9 @@
 -- nil represents an undefined value
 local a
 print(a)
+
+-- Declare a local variable named 'num' and assign it the value 20
+local num = 20
+
+-- Print the result of adding 1 to the value of 'num' (outputs 21)
+print(num + 1)
