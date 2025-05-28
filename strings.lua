81579e966c4ac40895a58dfe61b4dd24da088f5a
jazAlonso <xkaxolt@outlook.com>
2025-05-28 13:44:12 -0600

    add multiline string using double square brackets

--- strings.lua
+++ strings.lua
@@ -1,3 +1,12 @@
 -- Declare a local string variable and print ist data type
 local str = "Jazmin"
 print(type(str))
+
+
+-- Declare a multiline string using double square brackets
+local str1 = [[
+    My favorite food is Pozole
+    Pozole in Mexican Cuisine
+]]
+
+print(str1)
