f68cd45c9c4692d4a19ef6a90e10e48a43024929
jazAlonso <xkaxolt@outlook.com>
2025-05-27 18:51:09 -0600

    Update math.lua

--- math.lua
+++ math.lua
@@ -1,13 +1,16 @@
 -- str is a string variable, so the type will be "string"
+print("Type of variable")
 local str = "22"
 print(type(str))
 
 -- str1 is converted from string to number using tonumber, so the type will be "number"
+print("String to number")
 local str1 = "22"
 print(type(tonumber(str1)))
 
 
 -- Operations 
+print("Basic Operations")
 print(10 + 10 + 1)
 print(2023 - 10)
 print(2 * 2)
@@ -17,6 +20,7 @@ print(10 % 2)
 print((5 + 2) * 10 / 2)
 
 -- Adds the values of x and y , stores the result in z, then prints z
+print("Add two variables")
 x = 10
 y = 20
 
