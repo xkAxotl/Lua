dcee623c4bfcb4b868f288bb35bdeeb2584c03aa
jazAlonso <xkaxolt@outlook.com>
2025-05-27 18:37:28 -0600

    covert string to number and check its type

--- math.lua
+++ math.lua
@@ -1,3 +1,7 @@
 -- str is a string variable, so the type will be "string"
 local str = "22"
 print(type(str))
+
+-- str1 is converted from string to number using tonumber, so the type will be "number"
+local str1 = "22"
+print(type(tonumber(str1)))
