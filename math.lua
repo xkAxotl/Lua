854b038a3c89baccda0ed8b9a9bd75a9df1be08c
jazAlonso <xkaxolt@outlook.com>
2025-05-27 18:39:04 -0600

    simple addition of three numbers

--- math.lua
+++ math.lua
@@ -5,3 +5,7 @@ print(type(str))
 -- str1 is converted from string to number using tonumber, so the type will be "number"
 local str1 = "22"
 print(type(tonumber(str1)))
+
+
+-- Adds three numbers
+print(10 + 10 + 1)
